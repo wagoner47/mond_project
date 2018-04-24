532b242506cd3004d9fd4171f7c194390ecb68ae
wagoner47 <wagoner47@email.arizona.edu>
2018-04-24 15:58:04 -0700

    Created functions for gobs and gbar from data, and added to the necessary __init__.py files. Also changed the name of 'mond_test' directory to 'calculate'

--- mond_project/__init__.py
+++ mond_project/__init__.py
@@ -3,7 +3,7 @@ from __future__ import (division, unicode_literals, absolute_import,
 from ._version import __version__, __version_info__
 version = __version__
 
-from . import mond_test
-from .mond_test import *
+from . import calculate
+from .calculate import *
 from . import data_utils
 from .data_utils import *
--- mond_project/calculate/__init__.py
+++ mond_project/calculate/__init__.py
@@ -1,2 +1,3 @@
 from .._version import __version__, __version_info__
 version = __version__
+from .calc_accel import calc_gbar, calc_gobs
