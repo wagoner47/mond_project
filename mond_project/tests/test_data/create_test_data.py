20f57912986f655bb00724a24b1b00a0108d6ca7
Erika Wagoner <wagoner47@susie.physics.arizona.edu>
2018-05-03 15:59:02 -0700

    Update test data to only keep first 100 'galaxies'

--- mond_project/tests/test_data/create_test_data.py
+++ mond_project/tests/test_data/create_test_data.py
@@ -2,6 +2,7 @@ import mond_project
 import h5py
 import pandas as pd
 import numpy as np
+import os
 
 h = 0.704
 query_params = {"stars":"Coordinates,Masses,Velocities",
@@ -11,6 +12,11 @@ snap = mond_project.get(snap_url)
 a = 1.0 / (1.0 + snap["redshift"])
 file_base_z = "Illustris-1_z=0_subhalo{}.pickle.gz"
 file_base_sn = "Illustris-1_snapnum=135_subhalo{}.pickle.gz"
+target_dir = os.path.join(os.getcwd(), "exp")
+if os.path.isfile(target_dir):
+    os.rename(target_dir, target_dir + ".file")
+if not os.path.isdir(target_dir):
+    os.makedirs(target_dir)
 id_list = []
 subi = mond_project.get(snap["subhalos"])
 for halo in subi["results"]:
@@ -37,19 +43,19 @@ with h5py.File(file0, "r") as f:
     df = pd.DataFrame.from_dict({"r": np.append(r_gas, r_stars), "M":
         np.append(m_gas, m_stars), "v": np.append(v_gas, v_stars), "type":
         np.append(t_gas, t_stars)})
-    df.to_pickle(os.path.join(os.getcwd(), "exp",
+    df.to_pickle(os.path.join(target_dir,
         file_base_z.format(id_list[0])))
-    df.to_pickle(os.path.join(os.getcwd(), "exp",
+    df.to_pickle(os.path.join(target_dir,
         file_base_sn.format(id_list[0])))
-while subi["next"] is not None:
+while subi["next"] is not None and len(id_list) < 100:
     subi = mond_project.get(subi["next"])
     for halo in subi["results"]:
         haloi = mond_project.get(halo["url"])
         if haloi["mass_stars"] > 0.0 and haloi["mass_gas"] > 0.0:
             id_list.append(haloi["id"])
 file_list = [file_base_z.append(halo_id) for halo_id in id_list]
-np.savez_compressed(os.path.join(os.getcwd(), "exp", "subhalo_list_z.npz"),
+np.savez_compressed(os.path.join(target_dir, "subhalo_list_z.npz"),
         file_list)
 file_list = [file_base_sn.append(halo_id) for halo_id in id_list]
-np.savez_compressed(os.path.join(os.getcwd(), "exp", "subhalo_list_snapnum.npz"),
+np.savez_compressed(os.path.join(target_dir, "subhalo_list_snapnum.npz"),
         file_list)
